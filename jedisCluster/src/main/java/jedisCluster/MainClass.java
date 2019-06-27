433acfba21daf611a7dedd28077b4f058da69f21
shivanshu <shivanshu.s@globalpay.com>
2019-06-27 17:17:31 +0530

    using jedisShards

--- jedisCluster/src/main/java/jedisCluster/MainClass.java
+++ jedisCluster/src/main/java/jedisCluster/MainClass.java
@@ -1,15 +1,35 @@
 package jedisCluster;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisShardInfo;
+import redis.clients.jedis.ShardedJedis;
 
 public class MainClass {
 
 	public static void main(String[] args) {
+
+		List<JedisShardInfo> shards = new ArrayList<JedisShardInfo>();
+
+	    JedisShardInfo si = null;
+
+	    si = new JedisShardInfo("10.0.0.4", 6379);
+	    shards.add(si);
+	    si = new JedisShardInfo("10.0.0.12", 6379);
+	    shards.add(si);
+	    
+
+	    ShardedJedis jedis = new ShardedJedis(shards);
+	    System.out.println("pohonche kya shard setup tak????");
+	    //jedis.set("foo_first", "foo");
+		
+		
 		/*
 		 * System.out.println("shuruaat code ki"); Jedis jedis = new
 		 * Jedis("10.0.0.12"); System.out.println(
@@ -18,7 +38,7 @@ public class MainClass {
 		 * jedis.close();
 		 */
 
-		System.out.println("program shuruu ho gya maamu");
+		/*System.out.println("program shuruu ho gya maamu");
 		Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
 		jedisClusterNodes.add(new HostAndPort("10.0.0.4", 6379));
 		//jedisClusterNodes.add(new HostAndPort("10.0.0.12", 6379));
@@ -26,7 +46,7 @@ public class MainClass {
 		System.out.println("cluster node set initialised");
 		JedisCluster jedis = new JedisCluster(jedisClusterNodes);
 		System.out.println("this line is after creation of jedisCluster");
-
+*/
 		
 		/*
 		 * JedisCluster cluster - new JedisCluster(...); ... cluster.set(key,
