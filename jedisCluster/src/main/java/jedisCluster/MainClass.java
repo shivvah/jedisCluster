fd9ffb986788f797a22fb982769f4e493d29cf46
shivanshu <shivanshu.s@globalpay.com>
2019-06-27 17:12:37 +0530

    assuming memstore instance as a cluster

--- jedisCluster/src/main/java/jedisCluster/MainClass.java
+++ jedisCluster/src/main/java/jedisCluster/MainClass.java
@@ -21,7 +21,7 @@ public class MainClass {
 		System.out.println("program shuruu ho gya maamu");
 		Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
 		jedisClusterNodes.add(new HostAndPort("10.0.0.4", 6379));
-		jedisClusterNodes.add(new HostAndPort("10.0.0.12", 6379));
+		//jedisClusterNodes.add(new HostAndPort("10.0.0.12", 6379));
 
 		System.out.println("cluster node set initialised");
 		JedisCluster jedis = new JedisCluster(jedisClusterNodes);
